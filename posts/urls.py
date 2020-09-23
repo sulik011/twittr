ad56d72a0632290d84c46f36043875f5aee41159
Your Name <you@example.com>
2020-09-23 20:47:32 +0600

    comments

--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('posts/', views.PostAPIView.as_view),
-    path('postlist/<int:pk>/', views.PostDetail.as_view),
+    # path('postlist/<int:pk>/', views.PostDetail.as_view),
 ]
